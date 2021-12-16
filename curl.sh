d102d646c760d95a67a528bc8ba558b6e074b75c
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2021-12-16 11:24:05 +0100

    Implemented login and me query

--- curl.sh
+++ curl.sh
@@ -1,4 +1,5 @@
 
 
 curl -X POST -H "Content-Type: application/json" \
-    -d '{"username" : "stefano+olvg@waag.org", "password" : "PasswordOLVG1"}' localhost:1880/reflow
+    -d '{"username" : "stefano+olvg@waag.org", "password" : "PasswordOLVG1", "endpoint" : "http://135.181.35.156:4000/api/explore" }' \
+    localhost:1880/reflow
