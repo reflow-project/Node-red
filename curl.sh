ce181e614278eee3d2cdd030d405949468e31ca5
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2021-12-16 13:41:37 +0100

    Implemented create unit and create location

--- curl.sh
+++ curl.sh
@@ -2,4 +2,4 @@
 
 curl -X POST -H "Content-Type: application/json" \
     -d '{"username" : "stefano+olvg@waag.org", "password" : "PasswordOLVG1", "endpoint" : "http://135.181.35.156:4000/api/explore" }' \
-    localhost:1880/reflow
+    localhost:1880/amsterdam
